a0e507b8a8b926aeb234e024b226cdce06ba2243
Hanna Kaljaste <hakalj@ttu.ee>
2020-10-27 14:16:24 +0200

    added repositories

---
+++ Shop/Models/IItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models
+{
+    public interface IItemRepository
+    {
+        // all items and all itmes that are on sale
+        IEnumerable<Item> GetAllItem { get; }
+        IEnumerable<Item> GetItem { get; }
+        Item GetItemById(int itemId); //returns items by item id
+
+    }
+}
