dab26b8246a62edbb356a8d6c31857f27ecba542
Johanna Maria Prins <joprin@ttu.ee>
2020-10-27 11:57:39 +0200

    Add project files.

---
+++ Shop/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.Extensions.Hosting;
+
+namespace Shop
+{
+    public class Startup //startup 
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // called by .net core automatically 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(); // we have to add support to MVC
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+            //tells the app what environment it should run in
+            // IApplicationBuilder - tells we want the application to run in HTTPS
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage(); // allows us to display error messages - disable before you deploy the app live
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(); // will automatically search for directory wwwroot - enables images and js
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => // enable MVC to respond to request
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    ); // allows our app to route through the request and return a response
+            });
+        }
+
+    }
+}
